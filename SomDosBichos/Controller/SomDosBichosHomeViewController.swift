03bed18ffd04c170d5a9fb21a9cd8079bc080cfc
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-03-27 11:30:24 -0300

    [feat] - Implementa metodo de executar audio respectivo de cada botao

--- SomDosBichos/Controller/SomDosBichosHomeViewController.swift
+++ SomDosBichos/Controller/SomDosBichosHomeViewController.swift
@@ -6,10 +6,12 @@
 //
 import Foundation
 import UIKit
+import AVFoundation
 
 class SomDosBichosHomeViewController: UIViewController {
     
     private let contentView: SomDosBichosView = SomDosBichosView()
+    var player = AVAudioPlayer()
     
     
     override func viewDidLoad() {
@@ -25,7 +27,19 @@ class SomDosBichosHomeViewController: UIViewController {
     }
     
     private func setupContentView(){
+        let caoButton = contentView.caoButton
+        let gatoButton = contentView.gatoButton
+        let leaoButton = contentView.leaoButton
+        let macacoButton = contentView.macacoButton
+        let ovelhaButton = contentView.ovelhaButton
+        let vacaButton = contentView.vacaButton
         
+        caoButton.addTarget(self, action: #selector(playCao), for: .touchUpInside)
+        gatoButton.addTarget(self, action: #selector(playGato), for: .touchUpInside)
+        leaoButton.addTarget(self, action: #selector(playLeao), for: .touchUpInside)
+        macacoButton.addTarget(self, action: #selector(playMacaco), for: .touchUpInside)
+        ovelhaButton.addTarget(self, action: #selector(playOvelha), for: .touchUpInside)
+        vacaButton.addTarget(self, action: #selector(playVaca), for: .touchUpInside)
     }
     
     
@@ -43,4 +57,46 @@ class SomDosBichosHomeViewController: UIViewController {
             contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
+    
+    private func playSom(nomeSom: String){
+        
+        if let path = Bundle.main.path(forResource: nomeSom, ofType: "mp3") {
+            
+            let url = URL(fileURLWithPath: path)
+            
+            do {
+                try player = AVAudioPlayer(contentsOf: url)
+                player.prepareToPlay()
+                player.play()
+            } catch  {
+                print("Nao foi possivel reproduzir o audio! Erro: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    @objc private func playCao(){
+        playSom(nomeSom: "cao")
+    }
+    
+    @objc private func playGato(){
+        playSom(nomeSom: "gato")
+    }
+    
+    @objc private func playLeao(){
+        playSom(nomeSom: "leao")
+    }
+    
+    @objc private func playMacaco(){
+        playSom(nomeSom: "macaco")
+    }
+    
+    @objc private func playOvelha(){
+        playSom(nomeSom: "ovelha")
+    }
+    
+    @objc private func playVaca(){
+        playSom(nomeSom: "vaca")
+    }
+    
+   
 }
